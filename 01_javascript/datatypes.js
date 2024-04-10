0b13347f52aaaba3f084bcbf48c217940e1a6f59
ankitsaha00 <102477851+ankitsaha00@users.noreply.github.com>
2024-04-10 17:07:55 +0000

    datatypes

---
+++ 01_javascript/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+//alert (3 + 3)// we are using nodejs, not browser
+
+let name="Ankit"
+let age=22
+let isLoogedIn=false
+
+// number 
+// bigint
+// string =>""
+// boolean =>true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Ankit"); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null) // object
---
+++ 01_javascript/variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accoutEmail="abc@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+// accountId=2 //not allowed
+
+accoutEmail="xyz@gmail.com"
+accountPassword="12121212"
+accountCity="Raiganj"
+
+console.log(accountId);
+console.table([accountId,accoutEmail,accountPassword,accountCity])
