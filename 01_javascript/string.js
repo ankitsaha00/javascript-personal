bf29bec640cf254e9200b49339061d183a1f8d2f
ankitsaha00 <102477851+ankitsaha00@users.noreply.github.com>
2024-04-11 08:49:01 +0000

    string

---
+++ 01_javascript/string.js
@@ -0,0 +1,7 @@
+const gameName = new String('Ankit')
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
