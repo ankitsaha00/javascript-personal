9a1f6839edab44eb153b44d486caa4611189978d
ankitsaha00 <102477851+ankitsaha00@users.noreply.github.com>
2024-07-04 13:44:16 +0000

    array

---
+++ 01_javascript/array.js
@@ -0,0 +1,7 @@
+const myArray=[0,1,2,3,4,5];//different datatypes can possible
+//.push,.pop
+//unshift(first e value add)
+//shift
+//... spread operator
+//.flat(Infinity)//spread all
+//from ,of, array
